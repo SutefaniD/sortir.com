d76e2bf417cd5271aabafe17991ecded69a668b8
SutefaniD <110696367+SutefaniD@users.noreply.github.com>
2025-07-01 16:35:58 +0200

    Update Version20250701115426.php

--- migrations/Version20250701115426.php
+++ migrations/Version20250701115426.php
@@ -14,7 +14,7 @@ final class Version20250701115426 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'ajout zipcode dans City';
     }
 
     public function up(Schema $schema): void
