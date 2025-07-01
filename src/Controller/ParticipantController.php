f8e99f8f14385ea455b6ca9f4f05793ac7f731d6
Kevin29000 <kcorre444@gmail.com>
2025-07-01 11:56:19 +0200

    Gestion de la connexion

--- src/Controller/ParticipantController.php
+++ src/Controller/ParticipantController.php
@@ -8,11 +8,17 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/participant', name: 'participant_')]
 final class ParticipantController extends AbstractController
 {
+
+    public function __construct(private UserPasswordHasherInterface $passwordHasher)
+    {
+    }
+
     #[Route('/create', name: 'create')]
     public function create(EntityManagerInterface $entityManager, Request $request): Response
     {
@@ -22,6 +28,12 @@ final class ParticipantController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $plainPassword = $form->get('password')->getData();
+
+            $hashedPassword = $this->passwordHasher->hashPassword($participant, $plainPassword);
+
+            $participant->setPassword($hashedPassword);
+
             $participant->setAdministrator(false);
             $participant->setActive(true);
 
--- src/Entity/Participant.php
+++ src/Entity/Participant.php
@@ -5,9 +5,11 @@ namespace App\Entity;
 use App\Repository\ParticipantRepository;
 use Doctrine\ORM\Mapping as ORM;
 use phpDocumentor\Reflection\Types\Boolean;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: ParticipantRepository::class)]
-class Participant
+class Participant implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
@@ -116,5 +118,26 @@ class Participant
     public function setActive(?bool $active): void
     {
         $this->active = $active;
+    }
+
+    public function getRoles(): array
+    {
+        $roles = ['ROLE_USER'];
+
+        if ($this->administrator) {
+            $roles[] = 'ROLE_ADMIN';
+        }
+
+        return $roles;
+    }
+
+    public function eraseCredentials(): void
+    {
+        // TODO: Implement eraseCredentials() method.
+    }
+
+    public function getUserIdentifier(): string
+    {
+        return $this->email;
     }
 }
