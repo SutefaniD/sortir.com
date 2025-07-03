47d5b1e20396278c9ad2c573cb89d799a2113681
Kevin29000 <kcorre444@gmail.com>
2025-07-03 10:14:45 +0200

    CRUD Admin sur City et Site

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,9 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\City;
 use App\Entity\Participant;
+use App\Entity\Site;
+use App\Form\CityForm;
 use App\Form\ParticipantForm;
+use App\Form\SiteForm;
+use App\Repository\CityRepository;
 use App\Repository\ParticipantRepository;
+use App\Repository\SiteRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\FormError;
@@ -20,7 +26,7 @@ final class AdminController extends AbstractController
     {
     }
 
-    #[Route('/all-users', name: 'all_users')]
+    #[Route('/participant/all', name: 'all_users')]
     public function allUsers(ParticipantRepository $participantRepository): Response
     {
         $participants = $participantRepository->findAll();
@@ -28,7 +34,7 @@ final class AdminController extends AbstractController
         return $this->render('admin/all_participants.html.twig', ['participants' => $participants]);
     }
 
-    #[Route('/create-user', name: 'create_user')]
+    #[Route('/participant/create', name: 'create_user')]
     public function createUser(EntityManagerInterface $entityManager, Request $request): Response
     {
 //        return $this->redirectToRoute('participant_create');
@@ -61,7 +67,7 @@ final class AdminController extends AbstractController
         return $this->render('participant/create.html.twig', ['createParticipantForm' => $form]);
     }
 
-    #[Route('/delete/{id}', name: 'delete_user')]
+    #[Route('/participant/delete/{id}', name: 'delete_user')]
     public function delete_user(int $id, EntityManagerInterface $entityManager, ParticipantRepository $participantRepository): Response
     {
         $participant = $participantRepository->find($id);
@@ -76,7 +82,7 @@ final class AdminController extends AbstractController
         return $this->redirectToRoute('admin_all_users');
     }
 
-    #[Route('/disable-user/{id}', name: 'disable_user')]
+    #[Route('/participant/disable/{id}', name: 'disable_user')]
     public function disable_user(int $id, EntityManagerInterface $entityManager, ParticipantRepository $participantRepository): Response
     {
         $participant = $participantRepository->find($id);
@@ -98,5 +104,123 @@ final class AdminController extends AbstractController
         }
 
         return $this->redirectToRoute('admin_all_users');
+    }
+
+    #[Route('/city/all', name: 'city_all')]
+    public function all_cities(CityRepository $cityRepository): Response
+    {
+        $cities = $cityRepository->findAll();
+
+        return $this->render('admin/city/all.html.twig', ['cities' => $cities]);
+    }
+
+    #[Route('/city/create', name: 'create_city')]
+    public function create_city(EntityManagerInterface $entityManager, Request $request): Response
+    {
+        $city = new City();
+        $form = $this->createForm(CityForm::class, $city);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($city);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('admin_city_all');
+        }
+
+        return $this->render('admin/city/create.html.twig', ['cityForm' => $form]);
+    }
+
+    #[Route('/city/update/{id}', name: 'update_city')]
+    public function update_city(int $id, EntityManagerInterface $entityManager, Request $request, CityRepository $cityRepository): Response
+    {
+        $city = $cityRepository->find($id);
+        $form = $this->createForm(CityForm::class, $city);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($city);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('admin_city_all');
+        }
+
+        return $this->render('admin/city/create.html.twig', ['cityForm' => $form]);
+    }
+
+    #[Route('/city/delete/{id}', name: 'delete_city')]
+    public function delete_city(int $id, EntityManagerInterface $entityManager, CityRepository $cityRepository): Response
+    {
+        $city = $cityRepository->find($id);
+
+        if (!$city) {
+            throw $this->createNotFoundException('Ville non trouvé');
+        }
+
+        $entityManager->remove($city);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('admin_city_all');
+    }
+
+    #[Route('/site/all', name: 'site_all')]
+    public function all_site(SiteRepository $siteRepository): Response
+    {
+        $sites = $siteRepository->findAll();
+
+        return $this->render('admin/site/all.html.twig', ['sites' => $sites]);
+    }
+
+    #[Route('/site/create', name: 'create_site')]
+    public function create_site(EntityManagerInterface $entityManager, Request $request): Response
+    {
+        $site = new Site();
+        $form = $this->createForm(SiteForm::class, $site);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($site);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('admin_site_all');
+        }
+
+        return $this->render('admin/site/create.html.twig', ['siteForm' => $form]);
+    }
+
+    #[Route('/site/update/{id}', name: 'update_site')]
+    public function update_site(int $id, EntityManagerInterface $entityManager, Request $request, SiteRepository $siteRepository): Response
+    {
+        $site = $siteRepository->find($id);
+        $form = $this->createForm(SiteForm::class, $site);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($site);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('admin_site_all');
+        }
+
+        return $this->render('admin/site/create.html.twig', ['siteForm' => $form]);
+    }
+
+    #[Route('/site/delete/{id}', name: 'delete_site')]
+    public function delete_site(int $id, EntityManagerInterface $entityManager, SiteRepository $siteRepository): Response
+    {
+        $site = $siteRepository->find($id);
+
+        if (!$site) {
+            throw $this->createNotFoundException('Site non trouvé');
+        }
+
+        $entityManager->remove($site);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('admin_site_all');
     }
 }
